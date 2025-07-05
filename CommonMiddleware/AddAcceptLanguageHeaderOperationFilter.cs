371e8537560ae2c4c5e3317f4918f61e1a78c289
baki <baki@netadam.net>
2025-07-05 14:05:14 +0300

    z

---
+++ CommonMiddleware/AddAcceptLanguageHeaderOperationFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+
+public class AddAcceptLanguageHeaderOperationFilter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        // Accept-Language başlığını parametre olarak ekle
+        operation.Parameters ??= new List<OpenApiParameter>();
+
+        operation.Parameters.Add(new OpenApiParameter
+        {
+            Name = "Accept-Language",
+            In = ParameterLocation.Header,
+            Description = "Dil tercihinizi belirtin (örneğin, en-US, tr-TR)",
+            Required = false,
+            Schema = new OpenApiSchema
+            {
+                Type = "string",
+                Example = new OpenApiString("tr")
+            }
+        });
+    }
+}
--- CommonMiddleware/CustomSwaggerSetup.cs
+++ CommonMiddleware/CustomSwaggerSetup.cs
@@ -23,30 +23,8 @@ namespace CommonMiddleware
                     }
                 });
 
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
-                    In = ParameterLocation.Header,
-                    Description = "JWT Authorization header using the Bearer scheme. Example: Bearer {token}"
-                });
-
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        Array.Empty<string>()
-                    }
-                });
+               
+                c.OperationFilter<AddAcceptLanguageHeaderOperationFilter>();
             });
             return services;
         }
